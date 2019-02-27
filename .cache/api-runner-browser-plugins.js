6bdb55f5348bfbea2ee29c22635874af122a5308
paulapivat <paul.apivat@gmail.com>
2019-02-27 09:03:15 +0700

    first commit paul dev blog

---
+++ .cache/api-runner-browser-plugins.js
@@ -0,0 +1,4 @@
+module.exports = [{
+      plugin: require('/Users/paulapivat/Desktop/LambdaSchool/gatsby-starter-blog/paul-dev-blog/gatsby-browser.js'),
+      options: {"plugins":[]},
+    }]
---
+++ .cache/async-requires.js
@@ -0,0 +1,13 @@
+// prefer default export if available
+const preferDefault = m => m && m.default || m
+
+exports.components = {
+  "component---cache-dev-404-page-js": () => import("/Users/paulapivat/Desktop/LambdaSchool/gatsby-starter-blog/paul-dev-blog/.cache/dev-404-page.js" /* webpackChunkName: "component---cache-dev-404-page-js" */),
+  "component---src-pages-404-js": () => import("/Users/paulapivat/Desktop/LambdaSchool/gatsby-starter-blog/paul-dev-blog/src/pages/404.js" /* webpackChunkName: "component---src-pages-404-js" */),
+  "component---src-pages-index-js": () => import("/Users/paulapivat/Desktop/LambdaSchool/gatsby-starter-blog/paul-dev-blog/src/pages/index.js" /* webpackChunkName: "component---src-pages-index-js" */),
+  "component---src-pages-page-2-js": () => import("/Users/paulapivat/Desktop/LambdaSchool/gatsby-starter-blog/paul-dev-blog/src/pages/page-2.js" /* webpackChunkName: "component---src-pages-page-2-js" */),
+  "component---src-pages-page-3-js": () => import("/Users/paulapivat/Desktop/LambdaSchool/gatsby-starter-blog/paul-dev-blog/src/pages/page-3.js" /* webpackChunkName: "component---src-pages-page-3-js" */)
+}
+
+exports.data = () => import(/* webpackChunkName: "pages-manifest" */ "/Users/paulapivat/Desktop/LambdaSchool/gatsby-starter-blog/paul-dev-blog/.cache/data.json")
+
